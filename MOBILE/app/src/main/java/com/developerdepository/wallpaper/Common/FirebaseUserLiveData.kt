54f303b121e4c43e914fd93a02eaeb826cc8089b
triandut <trian7391@gmail.com>
2022-06-16 22:58:45 +0700

    login with google

---
+++ MOBILE/app/src/main/java/com/developerdepository/wallpaper/Common/FirebaseUserLiveData.kt
@@ -0,0 +1,33 @@
+package com.developerdepository.wallpaper.Common
+
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.FirebaseAuth
+import androidx.lifecycle.LiveData
+
+/**
+ * This class observes the current FirebaseUser. If there is no logged in user, FirebaseUser will
+ * be null.
+ *
+ * Note that onActive() and onInactive() will get triggered when the configuration changes (for
+ * example when the device is rotated). This may be undesirable or expensive depending on the
+ * nature of your LiveData object.
+ */
+class FirebaseUserLiveData : LiveData<FirebaseUser?>() {
+    private val firebaseAuth = FirebaseAuth.getInstance()
+
+    private val authStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+        value = firebaseAuth.currentUser
+    }
+
+    // When this object has an active observer, start observing the FirebaseAuth state to see if
+    // there is currently a logged in user.
+    override fun onActive() {
+        firebaseAuth.addAuthStateListener(authStateListener)
+    }
+
+    // When this object no longer has an active observer, stop observing the FirebaseAuth state to
+    // prevent memory leaks.
+    override fun onInactive() {
+        firebaseAuth.removeAuthStateListener(authStateListener)
+    }
+}
