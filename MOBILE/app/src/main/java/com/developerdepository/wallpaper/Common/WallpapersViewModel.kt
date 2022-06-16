f6c5eb5da83d5b686def2d399a3496866adec132
Toanil <congtoan.dn@gmail.com>
2022-06-16 19:12:36 +0700

    add wallpaper to sqlite

--- MOBILE/app/src/main/java/com/developerdepository/wallpaper/Common/WallpapersViewModel.kt
+++ MOBILE/app/src/main/java/com/developerdepository/wallpaper/Common/WallpapersViewModel.kt
@@ -1,19 +1,24 @@
 package com.developerdepository.wallpaper.Common
 
+import android.app.Application
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.firebase.firestore.DocumentSnapshot
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import com.developerdepository.wallpaper.Common.WallpapersModel
+import java.io.ByteArrayOutputStream
+import java.net.URL
+import java.nio.ByteBuffer
+import com.developerdepository.wallpaper.Common.Wallpaper
 
-class WallpapersViewModel : ViewModel() {
+class WallpapersViewModel() : ViewModel() {
 
     private val firebaseRepository: FirebaseRepository = FirebaseRepository()
+    public lateinit var application : Application
     private var page = 1
+    private lateinit var db : WallpaperDatabase
 
     private val wallpapersList: MutableLiveData<List<WallpapersModel>> by lazy {
         MutableLiveData<List<WallpapersModel>>().also {
@@ -26,27 +31,9 @@ class WallpapersViewModel : ViewModel() {
     }
 
     fun loadWallpapersData() {
-        Log.d("DEBUG4", "CALL API")
-//        API.apiService.wallpapers.enqueue(object: Callback<WallpapersList> {
-//            override fun onResponse(call: Call<WallpapersList>?, response: Response<WallpapersList>?) {
-//                if(response==null|| response.body()!!.wallpapersList ==null){
-//                    Log.d("DEBUG3", "CANT ACCESS API")
-//                    return
-//                }
-//
-//                if(wallpapersList.value == null) {
-//                    wallpapersList.value = response.body()!!.wallpapersList
-//                    wallpapersList.value!!.forEach {
-//                        Log.d("DEBUG1", it._id.toString())
-//                    }
-//                }
-//
-//            }
-//
-//            override fun onFailure(call: Call<WallpapersList>?, t: Throwable?) {
-//                Log.d("DEBUG3", t.toString())
-//            }
-//        })
+        db = WallpaperDatabase.getInstance(application)!!
+        val wallpaperDao = db!!.wallpaperDao()
+
         API.apiService.getWallpapers(page++, 6).enqueue(object: Callback<WallpapersList> {
             override fun onResponse(call: Call<WallpapersList>?, response: Response<WallpapersList>?) {
                 if(response==null|| response.body()!!.wallpapersList ==null){
@@ -58,12 +45,32 @@ class WallpapersViewModel : ViewModel() {
                     wallpapersList.value = response.body()!!.wallpapersList
                     wallpapersList.value!!.forEach {
                         Log.d("DEBUG1", it._id.toString())
+                        Thread {
+                            if(!wallpaperDao.isRowIsExist(it._id)) {
+                                val newWallpaper = Wallpaper(
+                                    it._id,
+                                    getByteArrayImage(it.data.large),
+                                    getByteArrayImage(it.data.small)
+                                )
+                                wallpaperDao.insertAll(newWallpaper)
+                            }
+                        }.start()
                     }
                 }
                 else {
                     response.body()!!.wallpapersList.forEach {
                         Log.d("DEBUG1", it._id.toString())
                         wallpapersList.value = wallpapersList.value!!.plus(it)
+                        Thread {
+                            if(!wallpaperDao.isRowIsExist(it._id)) {
+                                val newWallpaper = Wallpaper(
+                                    it._id,
+                                    getByteArrayImage(it.data.large),
+                                    getByteArrayImage(it.data.small)
+                                )
+                                wallpaperDao.insertAll(newWallpaper)
+                            }
+                        }.start()
                     }
                 }
 
@@ -73,5 +80,24 @@ class WallpapersViewModel : ViewModel() {
                 Log.d("DEBUG3", t.toString())
             }
         })
+    }
+
+    private fun getByteArrayImage(url: String): ByteArray {
+        val url = URL(url)
+        val output = ByteArrayOutputStream()
+        val conn = url.openConnection()
+        conn.setRequestProperty("User-Agent", "Firefox")
+
+        conn.getInputStream().use { inputStream ->
+            var n = 0
+            val buffer = ByteArray(1024)
+            while (-1 != inputStream.read(buffer).also { n = it }) {
+                output.write(buffer, 0, n)
+            }
+        }
+        val img: ByteArray = output.toByteArray()
+        val imageBytes = ByteBuffer.wrap(img)
+
+        return img
     }
 }
