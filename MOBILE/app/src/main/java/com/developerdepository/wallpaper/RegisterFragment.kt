ae466978d4787083b5b32a7ca18b879eb13a6e2d
triandut <trian7391@gmail.com>
2022-06-16 19:30:41 +0700

    login with google

--- MOBILE/app/src/main/java/com/developerdepository/wallpaper/RegisterFragment.kt
+++ MOBILE/app/src/main/java/com/developerdepository/wallpaper/RegisterFragment.kt
@@ -1,20 +1,30 @@
 package com.developerdepository.wallpaper
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment() {
 
     private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
     private var navController: NavController? = null
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,24 +38,34 @@ class RegisterFragment : Fragment() {
 
         navController = Navigation.findNavController(view)
 
-        register_feedback!!.setText(R.string.register_feedback_default)
+        btn_login.setOnClickListener { view: View? ->
+            login()
+        }
     }
-
     override fun onStart() {
         super.onStart()
-        navController!!.navigate(R.id.action_registerFragment_to_wallpapersCategoryFragment)
-//        if (firebaseAuth.currentUser == null) {
-//            register_feedback!!.setText(R.string.register_feedback1)
-//            firebaseAuth.signInAnonymously().addOnCompleteListener {
-//                if (it.isSuccessful) {
-//                    register_feedback!!.setText(R.string.register_feedback2)
-//                    navController!!.navigate(R.id.action_registerFragment_to_wallpapersCategoryFragment)
-//                } else {
-//                    register_feedback!!.setText(R.string.register_feedback3)
-//                }
-//            }
-//        } else {
-//            navController!!.navigate(R.id.action_registerFragment_to_wallpapersCategoryFragment)
-//        }
+
+    }
+    fun login(){
+        val email = edit_email.text.toString()
+        val pass = edit_pass.text.toString()
+        firebaseAuth!!.signInWithEmailAndPassword(email!!, pass!!)
+            .addOnCompleteListener(requireActivity()) { task ->  // <<< CHANGE WAS MADE HERE !
+                try{
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    println("createUserWithEmail:success")
+                    navController!!.navigate(R.id.action_registerFragment_to_wallpapersCategoryFragment)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "createUserWithEmail:failure", task.exception)
+                    println("Authentication failed.")
+
+                }}
+                catch (e:Exception){
+                    Log.d(e.toString() , "error")
+                }
+            }
     }
 }
